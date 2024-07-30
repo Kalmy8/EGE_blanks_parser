b44a9d4eec13f173f00d62a24170d4d5f2fbb932
Kalmy8 <kalmykovalexey01@gmail.com>
2024-07-30 16:02:58 +0500

    Refactor project

--- ege_parser/ege_blanks.py
+++ ege_parser/ege_blanks.py
@@ -1,61 +1,118 @@
 from __future__ import annotations
 
-from paddleocr import PaddleOCR, draw_ocr
-from paddleocr import PPStructure, save_structure_res, draw_structure_result
+import os
+import shutil
+from pathlib import Path
+
 import cv2 as cv2
 import numpy as np
-from src.utils import DataLoader
-from src.utils import Preprocessing
-import yaml
+from dotenv import load_dotenv
+from paddleocr import PPStructure, draw_structure_result, save_structure_res
 from PIL import Image
-from pathlib import Path
-import os
-# Upload config file constants
-with open('./configs/config.yaml', 'r', encoding='utf-8') as file:
-    config = yaml.safe_load(file)
 
-SCAN_PATH = Path(config['SCAN_PATH'])
-SAVE_FOLDER = config['SAVE_FOLDER']
-DET_MODEL_DIR = config['DET_MODEL_DIR']
-REC_MODEL_DIR = config['REC_MODEL_DIR']
-TABLE_MODEL_DIR = config['TABLE_MODEL_DIR']
-REC_CHAR_DICT_PATH = config['REC_CHAR_DICT_PATH']
-TABLE_CHAR_DICT_PATH = config['TABLE_CHAR_DICT_PATH']
-FONT_PATH = config['FONT_PATH']
+from ege_parser.utils import DataLoader, Preprocessing
+
+
+# Function to check and load missing environment variables
+def check_and_load_env_variables(required_env_variables):
+    # Check for missing environment variables
+    missing_args = [arg for arg in required_env_variables if os.getenv(arg) is None]
+
+    if missing_args:
+        # Print missing variables
+        for arg in missing_args:
+            print(f"Environment variable {arg} is not set")
+
+        # Load environment variables from .env file
+        print("Loading environmental variables from .env file...")
+        load_dotenv()
+
+        # Check again for missing variables after loading .env file
+        missing_args_after_load = [arg for arg in required_env_variables if os.getenv(arg) is None]
+
+        if missing_args_after_load:
+            # Print missing variables after attempting to load .env file
+            for arg in missing_args_after_load:
+                print(f"Environment variable {arg} is missing in .env file")
+
+            print("Please provide all required environmental variables and check the .env file")
+            raise ValueError("Missing required environmental variables")
+
+
+# Required variables list
+required_env_variables = [
+    "SCAN_PATH",
+    "SAVE_FOLDER",
+    "DET_MODEL_DIR",
+    "REC_MODEL_DIR",
+    "TABLE_MODEL_DIR",
+    "REC_CHAR_DICT_PATH",
+    "TABLE_CHAR_DICT_PATH",
+    "FONT_PATH",
+]
+
 
 class EGE_processor(Preprocessing):
-    def __call__(self, np_images : list | np.array) -> list | np.array:
-        if isinstance(np_images, np.ndarray):
-            return self._process_single_image(np_images)
-        elif isinstance(np_images, list):
-            return [self._process_single_image(np_image) for np_image in np_images]
-        else:
-            raise TypeError("Input must be a numpy array or a list of numpy arrays")
+    """
+    Child of Preprocessing class tuned to handle EGE-blanks related preprocessing tasks
+    """
 
-    def _process_single_image(self, np_image):
+    def __call__(
+        self, np_images: list[np.array], image_boundaries_reference: str | None = None
+    ) -> list[np.array]:
+        # Modify each entry of the original array
+        return [
+            self._process_single_image(np_image, image_boundaries_reference)
+            for np_image in np_images
+        ]
+
+    def _process_single_image(
+        self, np_image: np.array, image_template_path: str | None = None
+    ) -> np.array:
         np_image = Preprocessing.convert_to_grayscale(np_image)
-        np_image = Preprocessing.binary_threshold(np_image, n_neigbours=11, constant=2)
+        np_image = Preprocessing.binary_threshold(np_image, n_neigbours=21, constant=40)
 
-        # image_boundaries = Preprocessing.extract_image_grid(np_image,
-        #                                                     horiz_kernel_divider=10,
-        #                                                     vertic_kernel_divider=30,
-        #                                                     horiz_closing_iterations=3,
-        #                                                     vertical_closing_iterations=1)
+        # Extract image boundaries automatically if they are not provided
+        if image_template_path is not None:
+            try:
+                image_boundaries = cv2.imread(image_template_path)
 
-        image_boundaries = cv2.imread('./ege_grid.jpg')
+                # Preprocess template image
+                image_boundaries = cv2.cvtColor(image_boundaries, cv2.COLOR_BGR2GRAY)
+            except Exception as exc:
+                print(f"Can not upload {image_template_path} image_boundaries template\n", exc)
 
-        # Convert the image to grayscale
-        image_boundaries = cv2.cvtColor(image_boundaries, cv2.COLOR_BGR2GRAY)
+        else:
+            image_boundaries = Preprocessing.extract_image_grid(
+                np_image,
+                horiz_kernel_divider=10,
+                vertic_kernel_divider=30,
+                horiz_closing_iterations=3,
+                vertical_closing_iterations=1,
+            )
 
-        # Apply binary thresholding
+        # Binarize template image
         ret, image_boundaries = cv2.threshold(image_boundaries, 127, 255, cv2.THRESH_BINARY)
 
-        np_image = Preprocessing.extract_grid_entries(np_image, image_boundaries, verbose = True)
+        # Extract grid entries from the provided image
+        np_image = Preprocessing.extract_grid_entries(
+            np_image, image_boundaries, mode="inseparable", verbose=False
+        )
 
         return np_image
 
 
 def main():
+    check_and_load_env_variables(required_env_variables)
+
+    SCAN_PATH = Path(str(os.getenv("SCAN_PATH")))
+    SAVE_FOLDER = Path(str(os.getenv("SAVE_FOLDER")))
+    DET_MODEL_DIR = Path(str(os.getenv("DET_MODEL_DIR")))
+    REC_MODEL_DIR = Path(str(os.getenv("REC_MODEL_DIR")))
+    TABLE_MODEL_DIR = Path(str(os.getenv("TABLE_MODEL_DIR")))
+    REC_CHAR_DICT_PATH = Path(str(os.getenv("REC_CHAR_DICT_PATH")))
+    TABLE_CHAR_DICT_PATH = Path(str(os.getenv("TABLE_CHAR_DICT_PATH")))
+    FONT_PATH = Path(str(os.getenv("FONT_PATH")))
 
     # Load data
     data_loader = DataLoader()
@@ -64,24 +121,40 @@ def main():
     # Preprocess images
     engine = EGE_processor()
     data = dict((filename, engine(image)) for filename, image in data.items())
-
+    # , './data/external/ege_grid.jpg'
     # Initialize OCR engine
-    table_engine = PPStructure(det_model_dir=DET_MODEL_DIR,
-                               rec_model_dir=REC_MODEL_DIR,
-                               table_model_dir=TABLE_MODEL_DIR,
-                               rec_char_dict_path=REC_CHAR_DICT_PATH,
-                               table_char_dict_path=TABLE_CHAR_DICT_PATH,
-                               image_orientation=True,
-                               layout=True, show_log=True)
+    table_engine = PPStructure(
+        det_model_dir=DET_MODEL_DIR.__str__(),
+        rec_model_dir=REC_MODEL_DIR.__str__(),
+        table_model_dir=TABLE_MODEL_DIR.__str__(),
+        rec_char_dict_path=REC_CHAR_DICT_PATH.__str__(),
+        table_char_dict_path=TABLE_CHAR_DICT_PATH.__str__(),
+        image_orientation=True,
+        layout=True,
+        show_log=True,
+    )
 
-    for img in imgs:
-        result = table_engine(img)
-        save_structure_res(result, SCAN_PATH.joinpath(SAVE_FOLDER), file_path.name)
+    # Create the reports directory if it doesn't exist
+    SAVE_FOLDER.mkdir(parents=True, exist_ok=True)
 
-        image = Image.fromarray(img).convert('RGB')
-        im_show = draw_structure_result(image, result, font_path=FONT_PATH)
-        im_show = Image.fromarray(im_show)
-        im_show.show()
+    for filename, images_list in data.items():
+        dir_path = SAVE_FOLDER / filename
 
-if __name__ == '__main__':
+        if dir_path.exists():
+            # If it exists, delete it and recreate
+            shutil.rmtree(dir_path)
+            dir_path.mkdir(exist_ok=True)
+
+        # Assume every filename can have multiple images (like a multi-page PDF)
+        for page, image in enumerate(images_list):
+            result = table_engine(image)
+            save_structure_res(result, SCAN_PATH.joinpath(SAVE_FOLDER), f"{filename}_page_{page}")
+
+            image = Image.fromarray(image).convert("RGB")
+            im_show = draw_structure_result(image, result, font_path=FONT_PATH)
+            im_show = Image.fromarray(im_show)
+            im_show.show()
+
+
+if __name__ == "__main__":
     main()
--- ege_parser/utils.py
+++ ege_parser/utils.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
 
-import numpy as np
-import glob
 from pathlib import Path
-import os
+
 import cv2 as cv2
 import fitz
+import numpy as np
+
 
 class DataLoader:
-    '''
+    """
     Performs data loading and pdf mining
-    '''
+    """
+
     def __call__(self, scan_path):
         # Get file_paths
         file_paths = DataLoader.get_pdfs_and_images(scan_path)
@@ -21,30 +22,32 @@ class DataLoader:
             file_name = file_path.name
             file_format = file_path.suffix
 
-            if file_format == '.pdf':
+            if file_format == ".pdf":
                 imgs = DataLoader.mine_pdf(file_path)
             else:
-                imgs = cv2.imdecode(np.fromfile(file_path, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+                imgs = [cv2.imdecode(np.fromfile(file_path, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)]
 
             data[file_name] = imgs
 
         return data
 
     @staticmethod
-    def get_pdfs_and_images(scan_path : Path) -> list[Path]:
-        '''
-        Extracts all pdf/jpg/png files from specifies folder
+    def get_pdfs_and_images(scan_path: Path) -> list[Path]:
+        """
+        Extracts file paths of all pdf/jpg/png files from specifies folder
         :param scan_path: scan-folder path
         :return: all files that match specified extensions
-        '''
+        """
 
-        extensions = ['.pdf', '.png', '.jpg']
+        extensions = [".pdf", ".png", ".jpg"]
         try:
-            file_paths = [file for file in scan_path.glob('*') if file.suffix in extensions]
+            file_paths = [file for file in scan_path.glob("*") if file.suffix in extensions]
 
-        except:
-            print(f'No avaliable images or pngs found with SCAN_PATH={scan_path}\n',
-                   'Please verify the config.yaml file')
+        except Exception:
+            print(
+                f"No avaliable images or pngs found with SCAN_PATH={scan_path}\n",
+                "Please verify the config.yaml file",
+            )
 
         return file_paths
 
@@ -72,44 +75,49 @@ class DataLoader:
 
         return extracted_images
 
+
 class Preprocessing:
-    '''
+    """
     This class includes all preprocessing operations avaliable
-    '''
-    @staticmethod
-    def crop(np_image, x_crop : tuple[int, int], y_crop : tuple[int, int]) -> np.array:
-        return np_image[x_crop[0]:x_crop[1], y_crop[0]:y_crop[1], :]
+    """
 
     @staticmethod
-    def resize(np_image : np.array, scale_factor : float) -> np.array:
+    def crop(np_image, x_crop: tuple[int, int], y_crop: tuple[int, int]) -> np.array:
+        return np_image[x_crop[0] : x_crop[1], y_crop[0] : y_crop[1], :]
+
+    @staticmethod
+    def resize(np_image: np.array, scale_factor: float) -> np.array:
         # Resizing
-        return cv2.resize(np_image, (np_image.shape[1] * scale_factor, np_image.shape[0] * scale_factor),
-                          interpolation=cv2.INTER_CUBIC)
+        return cv2.resize(
+            np_image,
+            (np_image.shape[1] * scale_factor, np_image.shape[0] * scale_factor),
+            interpolation=cv2.INTER_CUBIC,
+        )
 
     @staticmethod
-    def gaussian_smooth(np_image : np.array, kernel_size: tuple[int, int]) -> np.array:
+    def gaussian_smooth(np_image: np.array, kernel_size: tuple[int, int]) -> np.array:
         return cv2.GaussianBlur(np_image, kernel_size)
 
     @staticmethod
-    def convert_to_grayscale(np_image : np.array):
+    def convert_to_grayscale(np_image: np.array):
         return cv2.cvtColor(np_image, cv2.COLOR_BGR2GRAY)
 
     @staticmethod
-    def binary_threshold(np_image : np.array, n_neigbours : int, constant : int) -> np.array:
+    def binary_threshold(np_image: np.array, n_neigbours: int, constant: int) -> np.array:
         # Apply binary thresholding with an adaptive method
-        return cv2.adaptiveThreshold(np_image, 255,
-                                     cv2.ADAPTIVE_THRESH_MEAN_C,
-                                     cv2.THRESH_BINARY,
-                                     n_neigbours,
-                                     constant)
+        return cv2.adaptiveThreshold(
+            np_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, n_neigbours, constant
+        )
 
     @staticmethod
-    def extract_image_grid(np_image: np.array,
-                           horiz_kernel_divider : int,
-                           vertic_kernel_divider : int,
-                           vertical_closing_iterations : int,
-                           horiz_closing_iterations : int) -> np.array:
-        '''
+    def extract_image_grid(
+        np_image: np.array,
+        horiz_kernel_divider: int,
+        vertic_kernel_divider: int,
+        vertical_closing_iterations: int,
+        horiz_closing_iterations: int,
+    ) -> np.array:
+        """
         Given numpy image, removes all objects excluding horizontal and vertical lines
 
         :param np_image: binarized/grayscaled numpy image
@@ -117,8 +125,8 @@ class Preprocessing:
         :param vertic_kernel_divider: greater the number -> smaller vertical lines will be extracted
         :param vertical_closing_iterations: greater the number -> cleaner the result
         :param horiz_closing_iterations: greater the number -> cleaner the result
-        :return: 
-        '''
+        :return:
+        """
 
         inversed = cv2.bitwise_not(np_image)
 
@@ -134,12 +142,20 @@ class Preprocessing:
 
         # Число итераций, чем больше число итераций, тем более явно производится очистка
         # Morphological operation to detect verticle lines from an image
-        verticle_lines_img = cv2.erode(verticle_lines_img, vertical_kernel, iterations= vertical_closing_iterations)
-        verticle_lines_img = cv2.dilate(verticle_lines_img, vertical_kernel, iterations= vertical_closing_iterations)
+        verticle_lines_img = cv2.erode(
+            verticle_lines_img, vertical_kernel, iterations=vertical_closing_iterations
+        )
+        verticle_lines_img = cv2.dilate(
+            verticle_lines_img, vertical_kernel, iterations=vertical_closing_iterations
+        )
 
         # Morphological operation to detect horizontal lines from an image
-        horizontal_lines_img = cv2.erode(horizontal_lines_img, horizontal_kernel, iterations= horiz_closing_iterations)
-        horizontal_lines_img = cv2.dilate(horizontal_lines_img, horizontal_kernel, iterations= horiz_closing_iterations)
+        horizontal_lines_img = cv2.erode(
+            horizontal_lines_img, horizontal_kernel, iterations=horiz_closing_iterations
+        )
+        horizontal_lines_img = cv2.dilate(
+            horizontal_lines_img, horizontal_kernel, iterations=horiz_closing_iterations
+        )
 
         # This function helps to add two image with specific weight parameter to get a third image as summation of two image.
         img_final = cv2.add(verticle_lines_img, horizontal_lines_img)
@@ -150,7 +166,9 @@ class Preprocessing:
         return img_final
 
     @staticmethod
-    def _sort_contours_and_hierarchy(contours: list | tuple, hierarchy: list) -> list:
+    def _sort_contours_and_hierarchy(
+        contours: list[np.array], hierarchy: list[np.array]
+    ) -> list[tuple[np.array]]:
         x_coords = []
         y_coords = []
         for cnt in contours:
@@ -158,14 +176,23 @@ class Preprocessing:
             x_coords.append(start_x)
             y_coords.append(start_y // 15)
         return list(
-            zip(*sorted(zip(contours, hierarchy, x_coords, y_coords), key=lambda t: (t[3], t[2]), reverse=False)))[:2]
+            zip(
+                *sorted(
+                    zip(contours, hierarchy, x_coords, y_coords),
+                    key=lambda t: (t[3], t[2]),
+                    reverse=False,
+                )
+            )
+        )[:2]
 
     @staticmethod
-    def extract_grid_entries(np_image: np.array,
-                             np_image_boundaries: np.array,
-                             mode: str = 'inseparable',
-                             verbose: bool = False) -> list[np.array]:
-        '''
+    def extract_grid_entries(
+        np_image: np.array,
+        np_image_boundaries: np.array,
+        mode: str,
+        verbose: bool = False,
+    ) -> list[np.array] | np.array:
+        """
         Extracts information within rectangle boundaries from given image using grid given by second argument
         :param np_image:
         :param mode: 'Mode' parameter stands for output list format:
@@ -173,14 +200,12 @@ class Preprocessing:
                      'inseperable' - return extracted regions as the only non-blacked-out regions on the original image
         :param verbose:
         :return: np.array
-        '''
-        data = []
-
-        # Loop through every image
-        regions_of_interest = []
+        """
 
         # Find contours using the standard Canny edge detection and contour finding pipeline
-        contours, hierarchy = cv2.findContours(np_image_boundaries, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(
+            np_image_boundaries, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        )
         hierarchy = [x for x in hierarchy[0]]
 
         # Sort contours from top to bottom left to right
@@ -189,23 +214,10 @@ class Preprocessing:
         # create an empty image for contours
         img_contours = np.uint8(np.zeros((np_image.shape[0], np_image.shape[1])))
 
-        # Check if contours are found
-        # if contours:
-        #     # Draw all contours with different parameters
-        #     for i, contour in enumerate(contours):
-        #         if len(contour) > 0:  # Ensure the contour is not empty
-        #             color = (i * 20 % 255, 255 - i * 10 % 255, i * 5 % 255)  # Unique color for each contour
-        #             cv2.drawContours(img_contours, contours, i, color, 2, cv2.LINE_AA)
-        # else:
-        #     print("No contours found.")
-        #
-        # cv2.imshow('a', img_contours)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
         # create blank list to hold ROI of original data
         blank_list = np.uint8(np.full((np_image.shape[0], np_image.shape[1]), 255))
 
+        regions_of_interest = []
         for cnt, hr in zip(contours, hierarchy):
             # I have no children, I have a parent and big enough
             if hr[2] == -1 and hr[-1] != -1 and cv2.contourArea(cnt) >= 400:
@@ -213,31 +225,38 @@ class Preprocessing:
                 # Margin for displaying only boxes content without box edges
                 margin = 2
                 if verbose:
-                    cv2.rectangle(img_contours, (x + margin, y + margin), (x + w - margin, y + h - margin),
-                                  (255, 255, 255), -1)
+                    cv2.rectangle(
+                        img_contours,
+                        (x + margin, y + margin),
+                        (x + w - margin, y + h - margin),
+                        (255, 255, 255),
+                        -1,
+                    )
 
-                roi = np_image[y + margin:y + h - margin, x + margin:x + w - margin]
+                roi = np_image[y + margin : y + h - margin, x + margin : x + w - margin]
 
                 # Fill blank List with areas of original image
-                blank_list[y + margin:y + h - margin, x + margin:x + w - margin] = roi
+                blank_list[y + margin : y + h - margin, x + margin : x + w - margin] = roi
 
                 regions_of_interest.append(roi)
 
-        if mode == 'piecewise':
-            data.append(regions_of_interest)
+        if mode == "piecewise":
+            output = regions_of_interest
 
-        elif mode == 'inseparable':
-            data.append(blank_list)
+        elif mode == "inseparable":
+            output = blank_list
 
         if verbose:
-            cv2.imshow('origin', np_image)  # Выводим оригинальное изображение
-            cv2.imshow('used grid', np_image_boundaries)
-            cv2.imshow('contours', img_contours)  # Выводим контуры
-            cv2.imshow('intersection', cv2.bitwise_and(np_image, img_contours))  # Выводим наложение
+            cv2.imshow("origin", np_image)  # Выводим оригинальное изображение
+            cv2.imshow("used grid", np_image_boundaries)
+            cv2.imshow("contours", img_contours)  # Выводим контуры
+            cv2.imshow(
+                "intersection", cv2.bitwise_and(np_image, img_contours)
+            )  # Выводим наложение
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
-        return data
+        return output
 
     @staticmethod
     def _visualize_bounding_boxes(image: np.array, reader_data):
@@ -255,14 +274,27 @@ class Preprocessing:
             top_left, _, bottom_right, _ = data[0]
             top_left, bottom_right = list(map(int, top_left)), list(map(int, bottom_right))
             # Draw rectangle on the image
-            cv2.rectangle(image, (top_left[0], top_left[1]), (bottom_right[0], bottom_right[1]), (0, 255, 0), 1)
+            cv2.rectangle(
+                image,
+                (top_left[0], top_left[1]),
+                (bottom_right[0], bottom_right[1]),
+                (0, 255, 0),
+                1,
+            )
 
             # Optionally, add text label under the bounding box
             text = data[1]
-            cv2.putText(image, text, (top_left[0], top_left[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+            cv2.putText(
+                image,
+                text,
+                (top_left[0], top_left[1] - 5),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.8,
+                (0, 255, 0),
+                2,
+            )
 
         # Display the image with bounding boxes
         cv2.imshow("Image with Bounding Boxes", image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
